6b56368ff065676dd3057a3a296d21a6bfd7b5bc
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-03-23 15:37:03 +0200

    create note body file

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -5,6 +5,13 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        title:Text('Notes'),
+        actions: [
+          IconButton( onPressed: () {  }, icon:Icon(Icons.search))
+          ],
+      ),
+    );
   }
 }
