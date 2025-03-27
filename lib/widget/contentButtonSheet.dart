aeb138c404b03c139d5324c195eb5336c0f7ba60
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-03-27 17:57:53 +0200

    take input and validate changes

--- lib/widget/contentButtonSheet.dart
+++ lib/widget/contentButtonSheet.dart
@@ -1,30 +1,69 @@
-
 import 'package:flutter/cupertino.dart';
 
 import 'Custombutton.dart';
 import 'customTextfield.dart';
 
-class contentButtonSheet extends StatelessWidget {
+class contentButtonSheet extends StatefulWidget {
   const contentButtonSheet({
     super.key,
   });
 
   @override
+  State<contentButtonSheet> createState() => _ContentButtonSheetState();
+}
+
+class _ContentButtonSheetState extends State<contentButtonSheet> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title;
+  String? con;
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 50,),
-            Customtextfield(hintText: 'Title'),
-            SizedBox(height:  30,),
-            Customtextfield(hintText: 'description',maxline: 5,),
-            SizedBox(height: 50,),
-            Custombutton(action: 'Add', onTap: () {  },),
-            SizedBox(height: 10,)
-        
-          ],
+        child: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              Customtextfield(
+                  onSaved: (value) {
+                    title = value;
+                  },
+                  hintText: 'Title'),
+              const SizedBox(
+                height: 30,
+              ),
+              Customtextfield(
+                hintText: 'description',
+                maxline: 5,
+                onSaved: (value) {
+                  con = value;
+                },
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Custombutton(
+                action: 'Add',
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/widget/customTextfield.dart
+++ lib/widget/customTextfield.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class Customtextfield extends StatelessWidget {
 
-  Customtextfield({super.key, required this.hintText,this.onChange,  this.obsec=false,  this.maxline=1});
+  Customtextfield({super.key, required this.hintText,this.onChange,  this.obsec=false,  this.maxline=1, this.onSaved});
 
 
   final String hintText;
@@ -10,11 +10,18 @@ class Customtextfield extends StatelessWidget {
   final bool? obsec;
   Function (String)? onChange;
 
-
+final void Function(String?)? onSaved;
 
   @override
   Widget build(BuildContext context) {
-    return  TextField(
+    return  TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if(value?.isEmpty ?? true){
+          return 'Field is required';
+        }
+        return null;
+      },
       maxLines: maxline,
       obscureText: obsec!,
       // validator: (data){
