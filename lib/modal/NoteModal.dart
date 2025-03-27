16fc215f986b2ef327699f24fbb1079e5a06cf6a
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-03-27 17:13:07 +0200

    generate note modal for hive

--- lib/modal/NoteModal.dart
+++ lib/modal/NoteModal.dart
@@ -1,12 +1,25 @@
-class NoteModal {
-  final String title;
-  final String subtitle;
-  final int color;
-  final String date;
+import 'package:hive/hive.dart';
 
-  const NoteModal(
-      {required this.title,
-      required this.subtitle,
-      required this.color,
-      required this.date});
+part 'NoteModal.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModal extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String subtitle;
+
+  @HiveField(2)
+  int color;
+
+  @HiveField(3)
+  String date;
+
+  NoteModal({
+    required this.title,
+    required this.subtitle,
+    required this.color,
+    required this.date,
+  });
 }
