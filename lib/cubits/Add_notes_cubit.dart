f5f07444ec46a4a63eda03ecd4b283f6b06dd837
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-04-12 00:18:21 +0200

    add node mothed in cubit

--- lib/cubits/Add_notes_cubit.dart
+++ lib/cubits/Add_notes_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app/modal/NoteModal.dart';
 
 part 'Add_notes_state.dart';
 
@@ -6,6 +8,20 @@ part 'Add_notes_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState>{
   AddNoteCubit(super.initialState);
+
+   AddNote(NoteModal note)async{
+
+     emit(AddNoteLoading());
+     try {
+       var notesBox =Hive.box<NoteModal>('notes_box');
+
+       emit(AddNoteSuccess());
+       await notesBox.add(note);
+
+     } on Exception catch (e) {
+      AddNoteFailure(e.toString());
+     }
+  }
 
 }
 
