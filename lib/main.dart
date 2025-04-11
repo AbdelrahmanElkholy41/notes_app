aa312c8693acb38ac6aabb400f3d7263fe0f68ef
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-04-11 15:38:52 +0200

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,25 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/screens/HomePage.dart';
 
-void main() async{
+import 'modal/NoteModal.dart';
+
+void main() async {
   await Hive.initFlutter();
   await Hive.openBox('notes_box');
+  Hive.registerAdapter(NoteModalAdapter());
   runApp(const NotesAPP());
 }
+
 class NotesAPP extends StatelessWidget {
   const NotesAPP({super.key});
 
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         //scaffoldBackgroundColor: Colors.
-
-
       ),
       home: Homepage(),
     );
