272907f72d09bccd4de5c217088ea76f70e36aef
Abdelrahman Elkhoily <abdelrahmanelkholy98@gmail.com>
2025-04-12 01:37:09 +0200

    test

--- lib/cubits/Add_notes_state.dart
+++ lib/cubits/Add_notes_state.dart
@@ -1,13 +1,14 @@
 part of 'Add_notes_cubit.dart';
 
-abstract class AddNoteState{
-}
+abstract class AddNoteState {}
 
+class AddNoteini extends AddNoteState {}
 
-class AddNoteini extends AddNoteState{}
-class AddNoteLoading extends AddNoteState{}
-class AddNoteSuccess extends AddNoteState{}
-class AddNoteFailure extends AddNoteState{
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
   final String errMessage;
   AddNoteFailure(this.errMessage);
 }
